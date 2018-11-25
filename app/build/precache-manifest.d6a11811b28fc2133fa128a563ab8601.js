ba64391261dbf22df0103c14529941d2dabbbaa6
Scott Saleeby <matthew_s_saleeby@homedepot.com>
2018-11-25 11:25:49 -0800

    move build/ into app/ for heroku

---
+++ app/build/precache-manifest.d6a11811b28fc2133fa128a563ab8601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b004787e180328af280",
+    "url": "/static/js/main.8b004787.chunk.js"
+  },
+  {
+    "revision": "144abcbcf6ebf6def3c3",
+    "url": "/static/js/1.144abcbc.chunk.js"
+  },
+  {
+    "revision": "8b004787e180328af280",
+    "url": "/static/css/main.56d1dd37.chunk.css"
+  },
+  {
+    "revision": "bdbeee4d6e8f90b13f8c1ef499432301",
+    "url": "/index.html"
+  }
+];
