074880044ae7c2594d41ee16e31180114cbc2ce0
Saleeby, Matthew S <matthew_s_saleeby@homedepot.com>
2018-11-30 16:52:50 -0500

    pull actual data, show in table.  chart up next

--- src/pages/history/BillsHistory.js
+++ src/pages/history/BillsHistory.js
@@ -1,42 +1,32 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 class BillsHistory extends Component {
     constructor(props) {
         super(props);
 
         this._getChart = this._getChart.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
+        this.onClick   = this.onClick.bind(this);
+        this._getTable = this._getTable.bind(this);
+        this.onStartChange = this.onStartChange.bind(this);
+        this.onEndChange = this.onEndChange.bind(this);
 
-    render() {
-        return (
-            <div style={{ padding: '20px' }}>
-                <TextField
-                    id="date"
-                    label="Start Date"
-                    type="date"
-                    defaultValue="2018-11-30"
-                    onChange={this.onChange}
-                />
-                <TextField
-                    id="date"
-                    label="End Date"
-                    type="date"
-                    defaultValue="2018-12-31"
-                    style={{marginLeft:'80px'}}
-                />
-                <div id="chart_div" style={{width: '500px', height: '400px', marginTop:'100px'}}></div>
-            </div>
-        );
-    }
+        this.state = {
+            showTable: false,
+            start: '11-29-2018',
+            end: '12-01-2018',
+            tableData: []
+        };
 
-    onChange() {
-        this._getChart();
-    }
-
-    _getChart() {
-        let info = [
+        this.data = [
             {
                 "_id": {
                     "$oid": "5bfdedebfb6fc006ff268fb2"
@@ -83,7 +73,105 @@ class BillsHistory extends Component {
                 "numberAmount": 0.5
             }
         ];
+    }
 
+    render() {
+        return (
+            <div style={{ padding: '20px' }}>
+                <TextField
+                    id="date"
+                    label="Start Date"
+                    type="date"
+                    defaultValue="2018-11-30"
+                    onChange={this.onStartChange}
+                />
+                <TextField
+                    id="date"
+                    label="End Date"
+                    type="date"
+                    defaultValue="2018-12-01"
+                    style={{marginLeft:'50px'}}
+                    onChange={this.onEndChange}
+                />
+                <Button variant="outlined" color="primary" style={{marginTop:'5px', marginLeft:'50px'}} onClick={this.onClick}>
+                    Get Data
+                </Button>
+                <div style={{marginTop: '100px'}}>
+                    {this.state.showTable ? this._getTable() : null}
+                    <div id="chart_div" style={{ width: '500px', height: '350px', display: 'inline-block', verticalAlign:'middle'}}></div>
+                </div>
+            </div>
+        );
+    }
+
+    onStartChange(evt) {
+        const d = evt.target.value.split('-');
+        this.setState({start: `${d[1]}-${d[2]}-${d[0]}`});
+    }
+
+    onEndChange(evt) {
+        const d = evt.target.value.split('-');
+        this.setState({ end: `${d[1]}-${d[2]}-${d[0]}` }, ()=> {console.log(this.state)});
+    }
+
+    onClick() {
+        let start = new Date(this.state.start).getTime();
+        let end = new Date(this.state.end).getTime();
+        const url = `https://api.mlab.com/api/1/databases/heroku_0lwkfbwj/collections/bills_history?q={epoch:{$lt:${end},$gt:${start}}}&apiKey=aVVSLiUK4fYFdptcpCwQR2sO9QXtZKXs`;
+        axios.get(url).then(resp => {
+            let info = {};
+            for(let history of resp.data) {
+                for(let bill of history.data) {
+                    if(info[bill.name]) {
+                        info[bill.name] += bill.numberAmount;
+                    }
+                    else {
+                        info[bill.name] = bill.numberAmount
+                    }
+                }
+            }
+
+            let data = [];
+            for(let k in info) {
+                data.push({
+                    name: k,
+                    value: info[k]
+                });
+            }
+
+            this.setState({ tableData: data, showTable: true });
+            this._getChart();
+        });
+    }
+
+    _getTable() {
+        return (
+            <Paper style={{width:'300px', display:'inline-block', verticalAlign:'middle', marginRight:'50px'}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell numeric>Amount Spent</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.tableData.map(row => {
+                            return (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell numeric>{row.value}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
+
+    _getChart() {
         var data = new window.google.visualization.DataTable();
         data.addColumn('string', 'Bills');
 
@@ -96,7 +184,7 @@ class BillsHistory extends Component {
             return color;
         }
 
-        let d = info.map(item => {
+        let d = this.data.map(item => {
             return [item.name, item.numberAmount, getRandomColor()];
         });
 
@@ -117,9 +205,6 @@ class BillsHistory extends Component {
         ]);
 
         var options = {
-            vAxis: {
-                title: 'Dollars'
-            },
             backgroundColor: 'transparent',
             legend: { position: "none" },
             bar: { groupWidth: "75%" },
